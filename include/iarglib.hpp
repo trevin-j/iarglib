5ba6baeaa5727a93bd5045fe9d980679ab07ac85
Trevin <trevindjones@gmail.com>
2022-06-14 00:15:44 -0600

    Created

---
+++ include/iarglib.hpp
@@ -0,0 +1,346 @@
+#ifndef IARGLIB_HPP
+#define IARGLIB_HPP
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+namespace iarglib
+{
+    // Declaration only
+    class IArger;
+
+    // Enum for specifying whether an option requires arguments or not.
+    enum class RequiresArgs
+    {
+        No,
+        Yes
+    };
+
+    // Struct for storing information about an option.
+    struct Option
+    {
+        // The name of the option.
+        std::string name;
+
+        // The identifiers for the option.
+        std::vector<std::string> identifiers;
+
+        // The description of the option.
+        std::string description;
+
+        // Whether or not the option requires arguments.
+        RequiresArgs requiresArgs;
+
+        // The function to call when the option is specified.
+        void (*func)(const IArger&);
+
+        // An argument passed to the option.
+        std::string argument;
+    };
+
+    // Declaration only
+    void printHelp(const IArger& arger);
+    void printVersion(const IArger& arger);
+
+    class IArger {
+
+    public:
+        // Construct the IArger object with the command line arguments.
+        IArger(int argc, char** argv)
+        {
+            this->argc = argc;
+            this->argv = argv;
+        }
+
+        // Set the application name. Used when displaying help or version info, if applicable.
+        // If addHelpOption() or addVersionOption() are used, it is recommended to use this too.
+        void setAppName(const std::string& appName)
+        {
+            this->appName = appName;
+        }
+
+        // Add a manual option to watch for.
+        // Unrecognized options and mistakes in the command line arguments will throw errors.
+        // Args:
+        //     optionName - The name of the option, used to check if it was passed in.
+        //     optionDescription - The description of the option, used when displaying help, and not required.
+        //     requiresArgs - Whether the option requires an argument. Use RequiresArgs::No for no and RequiresArgs::Yes for yes.
+        void addOption(const std::string& optionName, const std::string& identifiers, const std::string& helpMessage, RequiresArgs requiresArgs)
+        {
+            // Split the identifiers by the | character.
+            std::vector<std::string> identifierList = split(identifiers, '|');
+
+            Option option;
+            option.name = optionName;
+            option.identifiers = identifierList;
+            option.description = helpMessage;
+            option.requiresArgs = requiresArgs;
+
+            // Add the option to the map.
+            options[optionName] = option;
+
+            // Add the option to the list of all options.
+            allOptions.push_back(optionName);
+        }
+
+        // Add a manual option to watch for, and execute the specified function when the option is passed in.
+        // Unrecognized options and mistakes in the command line arguments will throw errors.
+        // Args:
+        //     optionName - The name of the option, used to check if it was passed in.
+        //     optionDescription - The description of the option, used when displaying help, and not required.
+        //     requiresArgs - Whether the option requires an argument. Use RequiresArgs::No for no and RequiresArgs::Yes for yes.
+        //     event - The function to execute when the option is passed in.
+        void addOptionEvent(const std::string& optionName, const std::string& identifier, const std::string& helpMessage, RequiresArgs requiresArgs, void (*eventFunction)(const IArger&))
+        {
+            // Split the identifiers by the | character.
+            std::vector<std::string> identifierList = split(identifier, '|');
+
+            Option option;
+            option.name = optionName;
+            option.identifiers = identifierList;
+            option.description = helpMessage;
+            option.requiresArgs = requiresArgs;
+            option.func = eventFunction;
+
+            // Add the option to the map.
+            options[optionName] = option;
+
+            // Add the option to the list of all options.
+            allOptions.push_back(optionName);
+
+            // Add the option to the list of options that have events.
+            eventOptions.push_back(optionName);
+        }
+
+        // Add a help option that prints the specified help message,
+        // the application name, and automatically generates a help screen from the option descriptions.
+        void addHelpOption(const std::string& helpMessage)
+        {
+            usingAutoHelp = true;
+            addOptionEvent("help", "-h|--help", "Display this help message", RequiresArgs::No, printHelp);
+        }
+
+        // Set the version of the app, and automatically display a version message when the version option is passed in.
+        void addVersionOption(const std::string& version)
+        {
+            usingAutoVersion = true;
+            addOptionEvent("version", "-v|--version", "Display the version of this application", RequiresArgs::No, printVersion);
+        }
+
+        // Set if the program should continue executing after displaying help info.
+        // This defaults to true so that you must explicitly be aware that false will stop parsing args after help.
+        // All this does is force stop parsing options if it finds the help option.
+        // Only works if using built-in help option.
+        void setContinueOnHelp(bool continueOnHelp)
+        {
+            this->continueOnHelp = continueOnHelp;
+        }
+
+        // Set if the program should continue executing after displaying version info.
+        // This defaults to true so that you must explicitly be aware that false will stop parsing args after version.
+        // All this does is force stop parsing options if it finds the version option.
+        // If the help option is passed and set to not continue on help, the version option will be ignored
+        // as the help option takes precedence.
+        // Only works if using built-in version option.
+        void setContinueOnVersion(bool continueOnVersion)
+        {
+            this->continueOnVersion = continueOnVersion;
+        }
+
+        // Parse the command line arguments. This is where events are triggered.
+        // Returns true if the program should continue executing, false if it should exit.
+        bool parse()
+        {
+            // Run through and map all option identifiers to their names.
+            for (auto& option : options)
+            {
+                for (auto& identifier : option.second.identifiers)
+                {
+                    identifiersToNames[identifier] = option.first;
+                }
+            }
+
+
+            int op = 1;
+            while (op < argc)
+            {
+                // Get the option.
+                std::string option = argv[op];
+                
+                // Get option's name from its identifier. If it's not an identifier, it's invalid.
+                auto optionNameIterator = identifiersToNames.find(option);
+
+                // If the option is invalid, throw an error.
+                if (optionNameIterator == identifiersToNames.end())
+                {
+                    throw std::runtime_error("Invalid option: " + option);
+                }
+
+                // get the option's name.
+                std::string optionName = optionNameIterator->second;
+
+                // Add option name to passedOptions.
+                passedOptions.push_back(optionName);
+
+                // Get the option from the map.
+                Option& optionData = options[optionName];
+
+                // If the option is help
+                if (optionName == "help")
+                {
+                    if (usingAutoHelp)
+                    {
+                        printHelp(*this);
+                        if (!continueOnHelp)
+                            return false;
+                    }
+                }
+
+                // If the option is version
+                if (optionName == "version")
+                {
+                    if (usingAutoVersion)
+                    {
+                        printVersion(*this);
+                        if (!continueOnVersion)
+                            return false;
+                    }
+                }
+
+                // If the option requires an argument, get the argument.
+                if (optionData.requiresArgs == RequiresArgs::Yes)
+                {
+                    op++;
+
+                    // If the next argument is non existent, throw an error.
+                    if (op >= argc)
+                    {
+                        throw std::runtime_error("Option " + optionName + " requires an argument.");
+                    }
+
+                    // Get the argument.
+                    optionData.argument = argv[op];
+
+                    // If the argument is empty, throw an error.
+                    if (optionData.argument == "")
+                    {
+                        throw std::runtime_error("Option " + optionName + " requires an argument.");
+                    }
+
+                    // If the argument is a different option, throw an error.
+                    if (std::count(allOptions.begin(), allOptions.end(), optionData.argument))
+                    {
+                        throw std::runtime_error("Option " + optionName + " requires an argument.");
+                    }
+                }
+
+                // If the option has an event, add it to the list of events to trigger at the end.
+                if (std::count(eventOptions.begin(), eventOptions.end(), optionName))
+                {
+                    triggerEvents.push_back(optionData.func);
+                }
+
+                op++;
+            }
+
+            // Run through each event option and trigger the event.
+            for (auto& event : triggerEvents)
+            {
+                event(*this);
+            }
+
+            return true;
+        }
+
+        // Get the names of all passed in options.
+        const std::vector<std::string>& getPassedOptions() const { return passedOptions; }
+
+        // Get all valid options.
+        const std::vector<std::string>& getAllOptions() const { return allOptions; }
+
+        // Check if a specific option was passed in.
+        bool optionExists(const std::string& optionName) const
+        {
+            return std::count(passedOptions.begin(), passedOptions.end(), optionName) > 0;
+        }
+
+        // Get the value of a specific option.
+        const std::string& getOptionArgument(const std::string& optionName) const
+        {
+            return options.at(optionName).argument;
+        }
+
+        // Get app name.
+        const std::string& getAppName() const { return appName; }
+
+
+    private:
+        // Arg count
+        int argc;
+        // Arg values
+        char** argv;
+        // Is the program using auto help/version?
+        bool usingAutoHelp = false;
+        bool usingAutoVersion = false;
+        // Continue on help/version?
+        bool continueOnHelp = true;
+        bool continueOnVersion = true;
+
+        // The application name.
+        std::string appName;
+
+        // Unordered map of options. Key is the option name, value is the option struct.
+        std::unordered_map<std::string, Option> options;
+
+        // List of all accepted options.
+        std::vector<std::string> allOptions;
+
+        // List of all event options.
+        std::vector<std::string> eventOptions;
+
+        // Vector to keep track of every option that was passed in.
+        std::vector<std::string> passedOptions;
+
+        // List of all passed event functions.
+        std::vector<void (*)(const IArger&)> triggerEvents;
+
+        // Map of option identifiers to their option names.
+        std::unordered_map<std::string, std::string> identifiersToNames;
+
+
+        // Function to split a string by a delimiter.
+        std::vector<std::string> split(std::string str, char delimiter) const
+        {
+            std::vector<std::string> tokens;
+            for (std::string::size_type i = 0; i < str.length(); i++)
+            {
+                if (str[i] == delimiter)
+                {
+                    tokens.push_back(str.substr(0, i));
+                    str.erase(0, i + 1);
+                    i = 0;
+                }
+            }
+            tokens.push_back(str);
+            return tokens;
+        }
+    };
+
+    void printHelp(const IArger& arger)
+    {
+        std::cout << "Help: " << arger.getAppName() << std::endl;
+        for (auto& option : arger.getAllOptions())
+        {
+            std::cout << "  " << option << std::endl;
+        }
+    }
+
+    void printVersion(const IArger& arger)
+    {
+        std::cout << "Version: " << arger.getAppName() << std::endl;
+    }
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "iarglib.hpp"
+
+using iarglib::IArger;
+using iarglib::RequiresArgs;
+
+void exampleDoSomething(const IArger& arger)
+{
+    std::cout << "The -d or --do-something option was specified." << std::endl;
+}
+
+void setupOptions(IArger& arger)
+{
+    // Set the application name. Used when displaying help or version info, if applicable.
+    arger.setAppName("IArgLib Example Application");
+
+    // Add help and version options.
+    // If added through this method, help and version messages are automatically generated and handled.
+    // You can also add help and version options manually using addOption() or addOptionEvent().
+    // With these special options, if continueOnHelp is false, every option beside help or version is ignored, even invalid options.
+    arger.addHelpOption("This IArgLib example shows how to use the IArger class to parse command line arguments.");
+    arger.addVersionOption("1.0.0");
+
+    // If using built-in help and version options, you can specify if you want the program to continue after displaying help or version info.
+    // These default to true so that you have to be aware that false will force the parser to stop after displaying help or version info,
+    // even though the program will still continue to run.
+    arger.setContinueOnHelp(false);
+    arger.setContinueOnVersion(false);
+
+    // Add some manual options.
+    arger.addOption("file", "-f|--file", "Example option - The file to read", RequiresArgs::Yes);
+    arger.addOption("output", "-o|--output", "Example option - The file to write", RequiresArgs::Yes);
+    arger.addOption("size", "-S|--size", "Example option - The size of the output file", RequiresArgs::Yes);
+    arger.addOption("read", "-r|--read", "Example option - Read the file", RequiresArgs::No);
+
+    arger.addOptionEvent("do-something", "-d|--do-something", "Example option - An example of an event option", RequiresArgs::No, exampleDoSomething);
+}
+
+int main(int argc, char** argv)
+{
+    // Initialize the IArger object.
+    IArger arger(argc, argv);
+    
+    // Set up the options.
+    setupOptions(arger);
+
+    bool keepRunning = false;
+
+    // It is up to the dev to determine how to handle stuff like invalid options
+    try
+    {
+        // Parse the command line arguments. This is where events are triggered.
+        keepRunning = arger.parse();
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    if (!keepRunning)
+    {
+        return 0;
+    }
+
+    // Get all options that were passed in.
+    std::vector<std::string> options = arger.getPassedOptions();
+
+    // Check if a specific option was passed in.
+    bool readOption = arger.optionExists("read");
+    if (readOption)
+    {
+        std::cout << "The -r or --read option was specified." << std::endl;
+    }
+
+    // Get the value of a specific option, if it was passed in.
+    if (arger.optionExists("file"))
+    {
+        std::string file = arger.getOptionArgument("file");
+        std::cout << "The file to read is: " << file << std::endl;
+    }
+}
